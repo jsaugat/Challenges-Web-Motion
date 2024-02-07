abed3bd5864fd4b43cfc39ce4b1b12bab720f6ea
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-08 01:59:18 +0545

    .

--- menu-overlay/components/Header/Button/index.jsx
+++ menu-overlay/components/Header/Button/index.jsx
@@ -1,36 +1,40 @@
-import { motion } from 'framer-motion';
-import styles from './button.module.scss';
+import { motion } from "framer-motion";
+import styles from "./button.module.scss";
 
-export default function index({isActive, toggleMenu}) {
+export default function index({ isActive, toggleMenu }) {
   return (
     <div className={styles.button}>
-        <motion.div 
-            className={styles.slider}
-            animate={{top: isActive ? "-100%" : "0%"}}
-            transition={{ duration: 0.4, type: "tween", ease: [0.76, 0, 0.24, 1]}}
+      <motion.div
+        className={styles.slider}
+        animate={{ top: isActive ? "-100%" : "0%" }}
+        transition={{ duration: 0.4, type: "tween", ease: [0.76, 0, 0.24, 1] }}
+      >
+        <div
+          className={styles.el}
+          onClick={() => {
+            toggleMenu();
+          }}
         >
-            <div 
-                className={styles.el}
-                onClick={() => {toggleMenu()}}
-            > <p>menu</p>
-                {/* <PerspectiveText label="Menu"/> */}
-            </div>
-            <div 
-                className={styles.el}
-                onClick={() => {toggleMenu()}}
-            > <p>close</p>
-                {/* <PerspectiveText label="Close" /> */}
-            </div>
-        </motion.div>
+          <PerspectiveText label="Menu" />
+        </div>
+        <div
+          className={styles.el}
+          onClick={() => {
+            toggleMenu();
+          }}
+        >
+          <PerspectiveText label="Close" />
+        </div>
+      </motion.div>
     </div>
-  )
+  );
 }
 
-// function PerspectiveText({label}) {
-//     return (    
-//         <div className={styles.perspectiveText}>
-//             <p>{label}</p>
-//             <p>{label}</p>
-//         </div>
-//     )
-// }
+function PerspectiveText({ label }) {
+  return (
+    <div className={styles.perspectiveText}>
+      <p>{label}</p>
+      <p>{label}</p>
+    </div>
+  );
+}
